ccbb4ddb4423a49cc560acd30fbcd9c563e2d5b8
Ronaldecheverry <ronaldecheverrry@gmail.com>
2020-09-07 22:48:15 -0500

    Adiccionamos css y modificamos index

--- src/main.js
+++ src/main.js
@@ -2,23 +2,44 @@ let addNoteBtn = document.querySelector('#addNotebtn');
 
 
 addNoteBtn.onclick = () => {
-//input forms
-let newNoteInput = document.querySelector('#newNoteInput');
-let notes =document.querySelector('#app .notes');
+    //input forms
+    let newNoteInput = document.querySelector('#newNoteInput');
+    let notes = document.querySelector('#app .notes');
 
 
-//New Elements
-let note = document.createElement('li');
-let checkbox = document.createElement('input');
+    //New Elements
+    let note = document.createElement('li');
+    let checkbox = document.createElement('input');
 
 
-checkbox.type = "checkbox";
-note.classList.add ('note');
+    checkbox.type = "checkbox";
 
-note.innerHTML = newNoteInput.value;
-note.appendChild(checkbox);
+    chechbox.onclick = function () {
 
-newNoteInput = '';
-notes.appendChild(note);
+        let parent = this.parentElement;
+
+        if (this.checked) {
+
+            parent.classList.add('isDone');
+
+
+        } else {
+
+            parent.classList.remove('isDone');
+        }
+
+
+    }
+
+
+
+
+    note.classList.add('note');
+
+    note.innerHTML = `<div>${newNoteInput.value}</div>`;
+    note.appendChild(checkbox);
+
+    newNoteInput = '';
+    notes.appendChild(note);
 
 }
